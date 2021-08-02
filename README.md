8f7fb5d2eac990ed5a14a8174414bdcc77d619b7
Lucida <luyiren12@yeah.net>
2021-08-02 14:11:12 +0800

    m

--- README.md
+++
@@ -1,1 +0,0 @@
-# buptcpc.github.io
--- calc.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let scores = {}
-
-for (let contestNo = 1; contestNo <= 10; ++contestNo) {
-
-}
-
-fs.writeFileSync(`sum.csv`, all);

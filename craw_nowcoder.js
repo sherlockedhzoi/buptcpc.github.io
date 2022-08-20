4a9edc85ebcb12ad59fe4901850d544361d75cce
Primy <wshprimy@gmail.com>
2022-08-20 17:04:11 +0800

    20

--- craw_nowcoder.js
+++ craw_nowcoder.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs')
 
-let all = [], ALL = 26, cid = 33194
+let all = [], ALL = 18, cid = 33195
 
 function work(cid) {
   let ranks = {}, i = 0
